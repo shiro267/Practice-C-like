faa8df9493df5016bc2c58dc9d6334ebd1c4af41
shiro267 <hyakugen0207@hotmail.com>
2018-05-24 11:50:26 +0800

    add c++ code of [Tower-of-Hanoi]

---
+++ Tower-Of-Hanoi/function.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void hanoi(int n, char A, char B, char C){
+
+    if(n==0)return;
+    hanoi(n-1,A,B,C);
+    printf("%d\n",n);
+    hanoi(n-1,B,C,A);
+
+    return;
+}
+
---
+++ Tower-Of-Hanoi/function.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+void hanoi(int n, char A, char B, char C);
+#endif
